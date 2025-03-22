92294095b45f0ddd94e8e0531980fa4b80518f6d
dishanth0102dr <dishanthraji2002@gmail.com>
2025-03-22 21:17:52 +0530

    Delete Day 02/Class Work directory

--- Day 02/Class Work/Ex 05/Typecasting01.java
+++
@@ -1,9 +0,0 @@
-public class Typecasting01{
-public static void main(String[] args){
-int num1 = 5004;
-double num2 = 2.5;
-double sum = num1+num2;
-
-System.out.println("the sum of "+num1+" and "+num2+" is "+sum);
-}
-}
